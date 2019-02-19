40bebb205bec40d088bc6a3d1dbf918d9876ff28
Alexander Guyer <guyera@oregonstate.edu>
2019-02-18 20:42:09 -0800

    Add CI tests for sonar

--- src/test/java/cs361/battleships/models/BoardTest.java
+++ src/test/java/cs361/battleships/models/BoardTest.java
@@ -34,7 +34,7 @@ public class BoardTest {
 		assertTrue(board.attack(0, 'A').getResult() == AtackStatus.INVALID);
 	}
 
-	
+
 	@Test
 	public void testRepeatedAttack(){
 		Board board = new Board();
@@ -46,7 +46,6 @@ public class BoardTest {
 	@Test
 	public void testSurrender() {
 		Board board = new Board();
-		System.out.println("NEXT TEST");
 		assertTrue(board.placeShip(new Ship("MINESWEEPER"), 10, 'A', false));
 		assertTrue(board.placeShip(new Ship("DESTROYER"), 9, 'A', false));
 		assertTrue(board.attack(10, 'A').getResult() == AtackStatus.HIT);
@@ -54,6 +53,33 @@ public class BoardTest {
 		assertTrue(board.attack(9, 'A').getResult() == AtackStatus.HIT);
 		assertTrue(board.attack(9, 'B').getResult() == AtackStatus.HIT);
 		assertTrue(board.attack(9, 'C').getResult() == AtackStatus.SURRENDER);
+	}
+
+	@Test
+	public void testSonar() {
+		Board board = new Board();
+		assertTrue(board.placeShip(new Ship("MINESWEEPER"), 10, 'A', false));
+		assertTrue(board.placeShip(new Ship("DESTROYER"), 9, 'A', false));
+		assertTrue(board.attack(10, 'A').getResult() == AtackStatus.HIT);
+		assertTrue(board.attack(9, 'A').getResult() == AtackStatus.HIT);
+		assertTrue(board.attack(9, 'C').getResult() == AtackStatus.HIT);
+		assertFalse(board.sonar(9, 'C'));//Out of bounds
+		assertFalse(board.sonar(8, 'B'));//Out of bounds
+		assertFalse(board.sonar(8, 'I'));//Out of bounds
+		assertFalse(board.sonar(2, 'C'));//Out of bounds
+		assertTrue(board.sonar(8, 'C'));
+		for(Result result : board.getAttacks()){
+				if(result.getLocation().equals(new Square(10, 'A')))
+						assertTrue(result.getResult() == AtackStatus.HIT);
+				else if(result.getLocation().equals(new Square(9, 'A')))
+						assertTrue(result.getResult() == AtackStatus.HIT);
+				else if(result.getLocation().equals(new Square(9, 'B')))
+						assertTrue(result.getResult() == AtackStatus.FOUND);
+				else if(result.getLocation().equals(new Square(9, 'C')))
+						assertTrue(result.getResult() == AtackStatus.HIT);
+				else
+						assertTrue(result.getResult() == AtackStatus.EMPTY);
+		}
 	}
 
 	//Miss tested in testRepeatedAttack
